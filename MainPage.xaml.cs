aa089e623bf1b73409a3b2c2308ba151d8696db6
CodyS80 <bartolosemolo@gmail.com>
2025-10-31 09:33:48 +0100

    Update 1
    Dodanie podstawowej struktury UI
    
    Dodanie szkieletu funkcji dla button'ów

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -2,24 +2,31 @@
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
-
+        new DateTime rightNow = DateTime.Now;
         public MainPage()
         {
+        
             InitializeComponent();
         }
-
-        private void OnCounterClicked(object sender, EventArgs e)
+        void registerEnter(object sender, EventArgs e)
         {
-            count++;
-
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
+            LblEnterTime.Text = rightNow.ToString("HH:mm");
         }
+        void registerExit(object sender, EventArgs e)
+        {
+            LblEnterTime.Text = " " + rightNow.ToString("HH:mm");
+        }
+        //private void OnCounterClicked(object sender, EventArgs e)
+        //{
+        //    count++;
+
+        //    if (count == 1)
+        //        CounterBtn.Text = $"Clicked {count} time";
+        //    else
+        //        CounterBtn.Text = $"Clicked {count} times";
+
+        //    SemanticScreenReader.Announce(CounterBtn.Text);
+        //}
     }
 
 }
