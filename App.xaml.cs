6e5fe6000ad8020ff1e9cbf0cce787f90b320fe7
CodyS80 <bartolosemolo@gmail.com>
2025-10-31 08:53:48 +0100

    Dodanie projektu

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace EwidencjaCzasuPracyMAUI
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
